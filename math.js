eeb44a4c69033561efb662cb98638146c9766c30
robsongajunior <robson.ga.junior@gmail.com>
2016-11-15 02:48:42 -0200

    [-] remove resolution code

--- math.js
+++ math.js
@@ -3,35 +3,27 @@ const expect = require('chai').expect
 // https://pt.wikipedia.org/wiki/Fatorial
 
 const fatorial = (numero) => {
-  let resultado = 1;
 
-  while(numero) {
-    resultado = resultado * numero;
-    numero--;
-  }
-
-  return resultado;
-};
+  return
+}
 
 describe('Fatorial',  () => {
   it('O fatorial de 5 deve ser 120', () => {
     const resultado = fatorial(5)
     expect(resultado).not.to.be.NaN
     expect(resultado).to.equal(120)
-  })
 
+  })
   it('O fatorial de 4 deve ser 24', () => {
     const resultado = fatorial(4)
     expect(resultado).not.to.be.NaN
     expect(resultado).to.equal(24)
   })
-  
   it('O fatorial de 3 deve ser 6', () => {
     const resultado = fatorial(3)
     expect(resultado).not.to.be.NaN
     expect(resultado).to.equal(6)
   })
-  
   it('O fatorial de 2 deve ser 2', () => {
     const resultado = fatorial(2)
     expect(resultado).not.to.be.NaN
